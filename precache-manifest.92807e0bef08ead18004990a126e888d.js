5a5a7eb8bc887eebd62fb7355f985d8e62ecd499
mbc-noh <hw.noh@mbc.co.kr>
2020-06-10 23:09:31 +0900

    Updates

---
+++ precache-manifest.92807e0bef08ead18004990a126e888d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970da22260060ea16896a48e1ab87089",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "23149da543351c83176e",
+    "url": "/movie_app_2020/static/css/main.3cd96663.chunk.css"
+  },
+  {
+    "revision": "89018a9f4d0393aaa4a7",
+    "url": "/movie_app_2020/static/js/2.6fa82bcd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.6fa82bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23149da543351c83176e",
+    "url": "/movie_app_2020/static/js/main.b6d4f49b.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
